c97132f01c09a6417ebe3dc292bb9a820db52f8d
Kevin Dixson <kdixson538@gmail.com>
2016-11-03 14:18:18 -0400

    Butterknife!

--- app/src/main/java/ajlyfe/thegiffingtree/MainActivity.java
+++ app/src/main/java/ajlyfe/thegiffingtree/MainActivity.java
@@ -3,11 +3,15 @@ package ajlyfe.thegiffingtree;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
