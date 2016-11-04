afaf8465550f26a550384d12459bc4dd58348306
Kevin Dixson <kdixson538@gmail.com>
2016-11-04 13:25:09 -0400

    Finish recycler view

--- app/src/main/java/ajlyfe/thegiffingtree/MainActivity.java
+++ app/src/main/java/ajlyfe/thegiffingtree/MainActivity.java
@@ -2,16 +2,36 @@ package ajlyfe.thegiffingtree;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerViewOptions;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        recyclerViewOptions = (RecyclerView) findViewById(R.id.recyclerViewOptions);
+        final OptionCardAdapter adapter = new OptionCardAdapter(getDummyArray(), this);
+        recyclerViewOptions.setAdapter(adapter);
+        recyclerViewOptions.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private ArrayList<OptionCard> getDummyArray() {
+        ArrayList<OptionCard> dummyArray = new ArrayList<>();
+
+        dummyArray.add(0, new OptionCard("Personal"));
+        dummyArray.add(1, new OptionCard("Professional"));
+
+        return dummyArray;
     }
 }
