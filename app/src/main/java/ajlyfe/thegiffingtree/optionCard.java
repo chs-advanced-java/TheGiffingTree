6f612a17a7975bc64abcf8094f8130e6a047ca1f
Kevin Dixson <kdixson538@gmail.com>
2016-11-04 13:39:00 -0400

    Start logic between options

--- app/src/main/java/ajlyfe/thegiffingtree/optionCard.java
+++ app/src/main/java/ajlyfe/thegiffingtree/optionCard.java
@@ -1,6 +1,7 @@
 package ajlyfe.thegiffingtree;
 
 import android.content.Context;
+import android.view.View;
 
 import java.util.ArrayList;
 
@@ -8,17 +9,28 @@ import ajlyfe.thegiffingtree.R;
 
 public class OptionCard {
     private String optionName;
+    private View.OnClickListener onClickListener;
 
-    public OptionCard(String optionName) {
+    public OptionCard(String optionName, View.OnClickListener onClickListener) {
         this.optionName = optionName;
+        this.onClickListener = onClickListener;
     }
 
     public String getOptionName() {
         return this.optionName;
     }
 
+    public View.OnClickListener getOnClickListener() {
+        return this.getOnClickListener();
+    }
+
     public OptionCard setOptionName(String optionName) {
         this.optionName = optionName;
+        return this;
+    }
+
+    public OptionCard setOnClickListener(View.OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
         return this;
     }
 }
